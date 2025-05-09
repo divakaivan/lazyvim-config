4f4f15345cbb09e5b90cc24c62595264607f7067
divakaivan <timee98@outlook.com>
2025-05-09 23:19:27 +0900

    final v2

---
+++ lua/plugins/noice.lua
@@ -0,0 +1,12 @@
+return {
+  "folke/noice.nvim",
+  opts = {
+    cmdline = {
+      view = "cmdline",
+      format = {
+        cmdline = { pattern = "^:", icon = ":", lang = "vim" },
+      },
+    },
+    presets = { command_palette = false },
+  },
+}
--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,31 +1,40 @@
 return {
-    "folke/snacks.nvim",
-    opts = {
-      picker = { enabled = false },
-        dashboard = {
-            sections = {{
-                section = "header"
-            }, {
-                icon = " ",
-                title = "Keymaps",
-                section = "keys",
-                indent = 2,
-                padding = 1
-            }, {
-                icon = " ",
-                title = "Recent Files",
-                section = "recent_files",
-                indent = 2,
-                padding = 1
-            }, {
-                icon = " ",
-                title = "Projects",
-                section = "projects",
-                indent = 2,
-                padding = 1
-            }, {
-                section = "startup"
-            }}
-        }
-    }
+  "folke/snacks.nvim",
+  opts = {
+    messages = {
+      enabled = false,
+    },
+    picker = { enabled = false },
+    dashboard = {
+      sections = {
+        {
+          section = "header",
+        },
+        {
+          icon = " ",
+          title = "Keymaps",
+          section = "keys",
+          indent = 2,
+          padding = 1,
+        },
+        {
+          icon = " ",
+          title = "Recent Files",
+          section = "recent_files",
+          indent = 2,
+          padding = 1,
+        },
+        {
+          icon = " ",
+          title = "Projects",
+          section = "projects",
+          indent = 2,
+          padding = 1,
+        },
+        {
+          section = "startup",
+        },
+      },
+    },
+  },
 }
